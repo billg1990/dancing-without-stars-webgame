538a24cb957cdc4b77cb9d401330d34888624815
Taikun Guo <billg1990@gmail.com>
2017-11-17 16:50:34 -0500

    Import full element ui and added about modal.

--- src/components/Greeting.vue
+++ src/components/Greeting.vue
@@ -1,19 +1,34 @@
 <template>
   <div class="Greeting">
-    <el-container>
-      <el-header>
-        <img style="width: 85%; height: 65%;" src="../assets/1200px-Dancing_without_the_Stars_Title_Logo.svg.png">
-      </el-header>
-      <el-main>
-        <h2>Please type your name here: </h2>
-        <el-input v-model="input" style="width: 30%" placeholder="e.g. Gandalf, Rambo"></el-input>
-        <el-button v-on:click="login" type="primary" style="background-color: #2F4F4F;">ENTER</el-button>
-      </el-main>
-      <el-footer>
-        <br />
-        <el-button v-on:click="about" type="text"><h3 style="color: #2c3e50">About</h3></el-button>
-      </el-footer>
-    </el-container>
+    <div>
+      <el-container>
+        <el-main>
+          <img style="width: 85%; height: 65%;" src="../assets/1200px-Dancing_without_the_Stars_Title_Logo.svg.png">
+        </el-main>
+        <el-main>
+          <h2>Please type your name here: </h2>
+          <el-input v-model="input" style="width: 30%" placeholder="e.g. Gandalf, Rambo"></el-input>
+          <el-button @click="login" type="primary" style="background-color: #2F4F4F;">ENTER</el-button>
+        </el-main>
+        <el-footer>
+          <br />
+          <el-button @click="aboutDialogVisible = true" type="text"><h3 style="color: #2c3e50">About</h3></el-button>
+          <h3 style="color: #2c3e50">Copyright 2017 Taikun Guo</h3>
+        </el-footer>
+      </el-container>
+    </div>
+    <!-- about modal -->
+    <div>
+      <el-dialog
+        title="About"
+        :visible.sync="aboutDialogVisible"
+        width="40%"
+        height="70%">
+        <div>This is my final project of 2017 Fall NYU Heuristic Problem Solving class.</div>
+        <div>License: MIT</div>
+        <div>Github: <a :href="githubUrl">{{ githubUrl }}</a></div>
+      </el-dialog>
+    </div>
   </div>
 </template>
 
@@ -23,10 +38,9 @@ export default {
   data () {
     return {
       input: '',
-      login: () => {
-        window.alert('')
-      },
-      about: () => {
+      githubUrl: 'https://github.com/billg1990/dancing-without-stars-webgame',
+      aboutDialogVisible: false,
+      login () {
         window.alert('')
       }
     }
