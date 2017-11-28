32035e32cba8f17222e20da078cb0fbd1b4d8917
Taikun Guo <billg1990@gmail.com>
2017-11-28 13:50:57 -0500

    Added Tile component.

---
+++ src/components/Tile.vue
@@ -0,0 +1,37 @@
+<template>
+  <el-button type="primary" class="Tile"
+    :style="{ width: this.width, height: this.height }"
+  >
+    <span :style="{ fontSize: '15vw', color: getContentColor(this.content) }">{{ this.content }}</span>
+  </el-button>
+</template>
+
+<script>
+export default {
+  name: 'Tile',
+  props: ['width', 'height', 'content'],
+  methods: {
+    getContentColor (content) {
+      const colors = ['red', 'blue', 'green', 'orange', 'purple', 'chocolate', 'pink', 'yellow']
+      if (content === '#' || content === '') {
+        return 'black'
+      }
+      let groupIndex = parseInt(content)
+      if (groupIndex <= 8) {
+        return colors[groupIndex - 1]
+      } else {
+        return 'black'
+      }
+    }
+  }
+}
+</script>
+
+<style>
+.Tile {
+  box-sizing: unset;
+  text-align: center;
+  font-weight: bold;
+  border:1px solid black;
+}
+</style>
