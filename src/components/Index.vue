257bd8fb0bc817a58aafbed430e799ebfad2a104
Taikun Guo <billg1990@gmail.com>
2017-11-29 10:43:52 -0500

    Pass player names to sub components.

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -2,9 +2,15 @@
   <div class="Index">
     <h2>Welcome {{ $route.params.name }}!</h2>
     <el-tabs type="border-card" v-model="activeName">
-      <el-tab-pane label="Play Games" name="play"><GameList></GameList></el-tab-pane>
-      <el-tab-pane label="Pose A Game" name="pose"><Pose></Pose></el-tab-pane>
-      <el-tab-pane label="How To Play" name="how"><How></How></el-tab-pane>
+      <el-tab-pane label="Play Games" name="play">
+        <GameList :player="$route.params.name"></GameList>
+      </el-tab-pane>
+      <el-tab-pane label="Pose A Game" name="pose">
+        <Pose :player="$route.params.name"></Pose>
+      </el-tab-pane>
+      <el-tab-pane label="How To Play" name="how">
+        <How :player="$route.params.name"></How>
+      </el-tab-pane>
     </el-tabs>
   </div>
 </template>
