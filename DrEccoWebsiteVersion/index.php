90fc47cafbeb19b48468caac175061ceeb1fdfb7
Taikun Guo <billg1990@gmail.com>
2017-12-03 21:10:26 -0500

    Added description on dr ecco site page.

--- DrEccoWebsiteVersion/index.php
+++ DrEccoWebsiteVersion/index.php
@@ -30,6 +30,36 @@
         <h3 id="groupName"></h3>
         <h3>Instruction:</h3>
         <div id="gameDesc" class="jumbotron">
+          <p>
+            On a <strong>N x N</strong> checker board, there are <strong>c</strong> groups of dancers in their initial positions. Each group will have a unique color and each group have <strong>k</strong> dancers.
+          </p>
+          <p>
+            The choreographer is going to ask the dancers to make moves. The goal of the choreographer is to ensure that there are disjoint, contiguous vertical or horizontal line segments of dancers.
+          </p>
+          <p>
+            They can be in any order, but one line should contain exactly one dancer of each color. A line can touch another line but they must be disjoint which means they can't share any dancer.
+          </p>
+          <p>
+            The spoiler is going to place <strong>k</strong> stars on the board before choreographer starts to move, and try make choreographer to spend more steps to reach a final state.
+          </p>
+          <p>
+            Both players will have <strong>120</strong> seconds as thinking time.
+          </p>
+          <p>
+            <strong>How to play</strong><br>
+            <strong>- click the button down below</strong><br>
+            <strong>- enter both players' names</strong><br>
+            <strong>- click GO! then spoiler starts to place stars</strong><br>
+            <strong>- spoiler can click on yellow tile to cancel a star</strong><br>
+            <strong>- spoiler click finish</strong><br>
+            <strong>- choreographer click dance! and start to move the dancers</strong><br>
+            <strong>- to move a dancer, click a dancer and click the target tile</strong><br>
+            <strong>- you can also cancel a dancing move</strong><br>
+            <strong>- click a dancer twice to make it stand still</strong><br>
+            <strong>- once you moved all the dancers, the game will increment step automatically</strong><br>
+            <strong>- once you succeed, the game will end automatically</strong><br>
+            <strong>- you can click on restart for another game</strong><br>
+          </p>
         </div>
       	<div id="scoreArea", class="jumbotron">
       	<?php
@@ -50,9 +80,7 @@
     <?php include $base."footer.php"; ?>
 </div>
 <script type="text/javascript">
-    initInfo("Dancing without the Stars 2017", "by TK", "TEST DESCRIPTION");
-    newTextBox("Spoiler","sName");
-    newTextBox("Choreographer", "cName");
+    initInfo("Dancing without the Stars 2017", "by TK", "");
     newWindowBtn(800,800,"./games/dancing2017/index.html", ['sName', 'cName']);
 </script>
 </body>
